38ef20374cd691c3abd108901c5e586757ff8721
vijethmoudgalya <vijethmoudgalya@gmail.com>
2024-12-15 11:58:06 +0530

    deployment changes3

--- src/langchain_utils.py
+++ src/langchain_utils.py
@@ -15,8 +15,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv('GROQ_API_KEY')
-# GROQ_API_KEY = st.secrets(['GROQ_API_KEY'])
+# GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+GROQ_API_KEY = st.secrets(['GROQ_API_KEY'])
 
 @st.cache_resource
 def get_chain(_example_selector):
